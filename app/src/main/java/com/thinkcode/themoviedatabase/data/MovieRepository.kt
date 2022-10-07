a7662aa0957524b033fb07e3da4a23426bd3802c
Cesar Yañez Martinez <>
2022-10-06 17:10:19 -0700

    Initial commit

---
+++ app/src/main/java/com/thinkcode/themoviedatabase/data/MovieRepository.kt
@@ -0,0 +1,4 @@
+package com.thinkcode.themoviedatabase.data
+
+class MovieRepository {
+}
---
+++ app/src/main/java/com/thinkcode/themoviedatabase/ui/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.thinkcode.themoviedatabase.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.thinkcode.themoviedatabase.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
